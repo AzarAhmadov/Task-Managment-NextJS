8ad724a2f3da1e5ffc424d9fcfb18e91d9d3f7af
AzarAhmadov <ehmedovh111@gmail.com>
2024-10-05 14:57:16 +0400

    Update Header.tsx

--- src/common/Header/Header.tsx
+++ src/common/Header/Header.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import { FaRegUserCircle, FaRegMoon } from "react-icons/fa";
 import { Button } from "@/components/ui/button";
 import { usePathname } from "next/navigation";
+import Logo from "../Logo/Logo";
 
 const Header = () => {
   const params = usePathname();
@@ -13,11 +14,14 @@ const Header = () => {
   return (
     <header className="py-4 px-5 bg-white border-b">
       <div className="container mx-auto flex items-center justify-between">
-        <div>
-          <h5 className="font-[500] text-lg">👋🏻 Welcome to TaskAza</h5>
-          <p className="font-[400] text-sm mt-1 text-textPrimary">
-            Please login or register to view your tasks
-          </p>
+        <div className="flex items-center gap-x-4">
+          <Logo />
+          <div>
+            <h5 className="font-[500] text-lg">👋🏻 Welcome to TaskAza</h5>
+            <p className="font-[400] text-sm mt-1 text-textPrimary">
+              Please login or register to view your tasks
+            </p>
+          </div>
         </div>
         <div className="flex items-center gap-x-3">
           <ul className="flex items-center gap-x-4">
