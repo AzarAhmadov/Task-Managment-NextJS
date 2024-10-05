bb82334ce8c698d0bf626a7ee62b19db06f455fc
AzarAhmadov <ehmedovh111@gmail.com>
2024-10-05 13:02:38 +0400

    add schemas.ts

---
+++ src/schemas/schemas.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+export const FormSchema = z.object({
+  email: z.string().min(1, { message: "Email is required" }).email({
+    message: "Email not valid",
+  }),
+  password: z.string().min(1, { message: "Password is required" }).min(5, {
+    message: "Password must be at least 5 characters long",
+  }),
+  name: z.string().min(1, { message: "Name is required" }).min(3, {
+    message: "Name must be at least 3 characters long",
+  }),
+});
