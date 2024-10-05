aff51fe2f3507f44cd38618b104559aef3849d46
AzarAhmadov <ehmedovh111@gmail.com>
2024-10-05 14:23:24 +0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
+import Container from "@/common/Container/Container";
+import Reyting from "@/components/Reyting/Reyting";
+import Tasks from "@/components/Task/Tasks";
+import TaskTop from "@/components/Task/TaskTop";
 import React from "react";
 
 const Home = () => {
-  return <></>;
+  return (
+    <Container>
+      <div className=" grid grid-cols-12 gap-x-7">
+        <div className="col-span-9 shadow-sm rounded-[25px] bg-[#f9f9f997] overflow-auto px-5 pb-5 h-[calc(100vh_-_135px)]">
+          <div className=" sticky py-5 top-0 bg-[#f9f9f9] z-20">
+            <TaskTop />
+          </div>
+          <Tasks />
+        </div>
+        <div className="col-span-3 bg-red-300 p-5 h-[calc(100vh_-_135px)]">
+          <Reyting />
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 export default Home;
