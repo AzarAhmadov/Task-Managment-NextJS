eca7442f5bff792367bb06c62d50eb7d0fd466fd
AzarAhmadov <ehmedovh111@gmail.com>
2024-10-05 13:01:51 +0400

    Create FormTop.tsx

---
+++ src/components/Form/FormTop.tsx
@@ -0,0 +1,24 @@
+import { TypeFormTop } from "@/types/types";
+import Link from "next/link";
+import React from "react";
+
+const FormTop: React.FC<TypeFormTop> = ({
+  title,
+  description,
+  href,
+  linkText,
+}) => {
+  return (
+    <div className="text-center mb-7">
+      <h4 className="text-lg font-[600]">{title}</h4>
+      <p className="text-sm mt-3 font-[400] text-[#00000072]">
+        {description}
+        <Link className="text-black font-[500] ms-1" href={href}>
+          {linkText}
+        </Link>
+      </p>
+    </div>
+  );
+};
+
+export default FormTop;
