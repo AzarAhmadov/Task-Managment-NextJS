e1c26d91c570c1698b33ac3467fd09f36fd86037
AzarAhmadov <ehmedovh111@gmail.com>
2024-10-05 13:02:41 +0400

    Update types.ts

---
+++ src/types/types.ts
@@ -0,0 +1,12 @@
+import { ReactNode } from "react";
+
+export type TypeContainer = {
+  children: ReactNode;
+};
+
+export type TypeFormTop = {
+  title: string;
+  description: string;
+  href: string;
+  linkText: string;
+};
