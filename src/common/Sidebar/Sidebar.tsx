f723b4e9d9292f31fe98b2e29f57ed3ffec81625
AzarAhmadov <ehmedovh111@gmail.com>
2024-10-05 14:23:21 +0400

    Update Sidebar.tsx

--- src/common/Sidebar/Sidebar.tsx
+++ src/common/Sidebar/Sidebar.tsx
@@ -26,10 +26,13 @@ const Sidebar = () => {
 
   return (
     <aside className="container mx-auto">
-      <ul className="space-y-10 top-[125px] fixed h-screen">
+      <ul className="space-y-10 top-[135px] fixed h-screen">
         {sidebarLinks.map((item, index) => (
           <li className="flex group items-center gap-x-3 w-[25px]" key={index}>
-            <Link href={"/"} className="text-[25px] hover:scale-105 transition-all">
+            <Link
+              href={"/"}
+              className="text-[25px] hover:scale-105 transition-all"
+            >
               {item.icon}
             </Link>
             <p className="bg-black invisible opacity-0 group-hover:visible -ml-1 group-hover:-ml-0   group-hover:opacity-100 transition-all duration-300 text-white rounded-full py-1 px-3 text-[14px]">
