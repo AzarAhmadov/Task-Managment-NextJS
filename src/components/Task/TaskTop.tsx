1d9d6a35e7941f3dc21163513020674632defe31
AzarAhmadov <ehmedovh111@gmail.com>
2024-10-20 14:01:47 +0400

    Update TaskTop.tsx

--- src/components/Task/TaskTop.tsx
+++ src/components/Task/TaskTop.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import { useAppContext } from "@/context/AppContext";
+import { firstTextStartUppercase } from "@/helper/helper";
+import { useParams } from "next/navigation";
 import React, { useState } from "react";
-import { date } from "zod";
 
 const TaskTop = () => {
   const tasksBtn = [
@@ -21,7 +22,7 @@ const TaskTop = () => {
   ];
 
   const [tab, setTab] = useState("all");
-
+  const { slug } = useParams();
   const { filterTaskLevel } = useAppContext();
 
   const handleButton = (data: any) => {
@@ -31,7 +32,9 @@ const TaskTop = () => {
 
   return (
     <div className="flex justify-between items-center">
-      <h4 className="text-xl font-[600]">All Tasks</h4>
+      <h4 className="text-xl font-[600]">
+        {firstTextStartUppercase(slug?.toString()) || "All"} Tasks
+      </h4>
 
       <div className="flex gap-x-1 bg-gradient-to-r from-gray-800 to-stone-800 p-2 text-white rounded-full text-sm">
         {tasksBtn.map((item, index) => (
@@ -42,7 +45,7 @@ const TaskTop = () => {
               tab === item.title ? "bg-white text-black " : null
             } px-5 rounded-full py-1 max-w-[100px] transition-all`}
           >
-            {item.title}
+            {firstTextStartUppercase(item?.title)}
           </button>
         ))}
       </div>
